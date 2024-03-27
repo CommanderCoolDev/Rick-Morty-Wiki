6b05f81533abc6cee79b6f8f42b765899806f015
Dima Glebov <78321515+CommanderCoolDev@users.noreply.github.com>
2024-03-27 23:54:04 +0200

    set structure

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import 'bootstrap/dist/js/bootstrap'
 function App () {
   return (
     <div className='App'>
-      <h1 className='text-center'>Rick and Morty Mighty WIKI</h1>
+      <h1 className='text-center ubuntu my-4'>
+        Rick and Morty <span className='text-primary'>Mighty WIKI</span>
+      </h1>
     </div>
   )
 }
