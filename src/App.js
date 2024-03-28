b3befce6e9f94744fd99ff3dafd399c8620c6cf0
Dima Glebov <78321515+CommanderCoolDev@users.noreply.github.com>
2024-03-28 23:34:24 +0200

    start adding layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap'
+import Filters from './components/Filters/Filters'
 // import './App.css'
 
 function App () {
@@ -8,6 +9,15 @@ function App () {
       <h1 className='text-center ubuntu my-4'>
         Rick and Morty <span className='text-primary'>Mighty WIKI</span>
       </h1>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-3'>
+            <Filters />
+          </div>
+          <div className='col-8'></div>
+          <div className='col-3'></div>
+        </div>
+      </div>
     </div>
   )
 }
