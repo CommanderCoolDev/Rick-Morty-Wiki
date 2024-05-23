eaa0dc945bd6509dd72e1930474b714761293f96
Dima Glebov <78321515+CommanderCoolDev@users.noreply.github.com>
2024-05-23 10:03:52 +0300

    episodes

--- src/Pages/Episodes.js
+++ src/Pages/Episodes.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 const Episodes = () => {
+  let [id, setID] = useState(1);
+  let [info, setInfo] = useState([])
+  let {air_date, name } = info
+  let api = `https://rickandmortyapi.com/api/episode/${id}`
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then(res => res.json())
+      setInfo(data)
+    })()
+  },[api])
   return <div>Episodes</div>
 }
 
