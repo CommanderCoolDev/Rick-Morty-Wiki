b5b3f99d4824212275a558928f2e7492e6bf6866
Dima Glebov <78321515+CommanderCoolDev@users.noreply.github.com>
2024-05-27 21:24:36 +0300

    add episodes

---
+++ src/components/Filters/Category/InputGroup.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function InputGroup({ total, name, setID }) {
+  return (
+    <div class='input-group mb-3'>
+      <select
+        onChange={e => setID(e.target.value)}
+        class='form-select'
+        id={name}
+      >
+        <option selected>Choose {name}...</option>
+        {[...Array(total).keys()].map(x => {
+          return (
+            <option value={x + 1}>
+              {name} - {x + 1}
+            </option>
+          )
+        })}
+      </select>
+    </div>
+  )
+}
+
+export default InputGroup
