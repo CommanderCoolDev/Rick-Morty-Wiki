86a0d9764dcec966e7741d18fbf8cfc1f757eebf
Dima Glebov <78321515+CommanderCoolDev@users.noreply.github.com>
2024-06-13 14:37:38 +0300

    add responsivenes

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -12,8 +12,8 @@ const Cards = ({ results, page }) => {
       return (
         <Link style={ 
           {textDecoration: 'none'}
-        } to={`${page}${id}`} key={id} className='col-4 mb-4 position-relative text-dark'>
-          <div className={s.cards}>
+        } to={`${page}${id}`} key={id} className='col-lg-4 col-md-6  col-12 mb-4 position-relative text-dark'>
+          <div className={`${s.cards} d-flex flex-column justify-content-center`}>
             <img src={image} alt='' className={`${s.img} img-fluid`} />
             <div className={`${s.content} content`}>
               <div className='fs-4 fw-bold mb-4'>{name}</div>
--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,35 +1,50 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import ReactPaginate from 'react-paginate'
 import s from './Pagination.module.scss'
 
 const Pagination = ({ setPageNumber, pageNumber, info }) => {
+  let[width, setWidth] = useState(window.innerWidth)
+  const updtDimension = () => {
+    setWidth(window.innerWidth)
+  }
+  useEffect(() => {
+    window.addEventListener('resize', updtDimension)
+    return () =>  window.removeEventListener('resize', updtDimension)
+  },[])
   return (
+    <>
+      <style jsx>
+        {`@media (max-width:768px){
+.next, .prev{
+  display:none;
+}
+.pagination{
+font-size: 14px;
+}
+        }`}
+    </style>
     <ReactPaginate
       pageCount={info?.pages}
       className='pagination  justify-content-center gap-3 my-4'
       nextLabel='Next'
       previousLabel='Prev'
-      nextClassName='btn btn-primary'
-      previousClassName='btn btn-primary'
+      nextClassName='btn btn-primary next'
+      previousClassName='btn btn-primary prev'
       previousLinkClassName={s.anchor}
       nextLinkClassName={s.anchor}
       pageClassName='page-item'
-      pageLinkClassName='page-link'
+        pageLinkClassName='page-link'
+        marginPagesDisplayed={width < 576 ? 1 :2}
+        pageRangeDisplayed={width < 576 ? 3 :5}
       activeClassName='active'
       // breakLabel='gogogo'
       forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
       onPageChange={data => {
         setPageNumber(data.selected + 1)
       }}
-    />
-    // <div className='container d-flex justify-content-center gap-5 my-5'>
-    //   <button className='btn btn-primary' onClick={prev}>
-    //     Prev
-    //   </button>
-    //   <button className='btn btn-primary' onClick={next}>
-    //     Next
-    //   </button>
-    // </div>
+      />
+      </>
+
   )
 }
 
